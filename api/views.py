277562995567bd52e85ccaddfbd2b882c7bb7f17
johnmuthua <caveomuthama@gmail.com>
2022-09-14 13:32:25 +0300

    feat:added user views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generics
 
+from api import serializers
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = serializers.UserSerializer
 # Create your views here.
